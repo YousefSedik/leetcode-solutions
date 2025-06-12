b5b682cfc0b09435d75a33a84bf58d86d311df0d
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-06-13 00:40:09 +0300

    [3423] [Time Beats: 100.00%] [Memory Beats: 100.00%] - LeetPush

--- Maximum-Difference-Between-Adjacent-Elements-in-a-Circular-Array.go
+++ Maximum-Difference-Between-Adjacent-Elements-in-a-Circular-Array.go
@@ -1,8 +1,7 @@
 func maxAdjacentDistance(nums []int) int {
-    // absolute(value) = max(value, value * -1)
-    answer := max(nums[0] - nums[len(nums) - 1], (nums[0] - nums[len(nums) - 1]) * -1)
+    answer := max(math.Abs(float64(nums[0] - nums[len(nums) - 1])))
     for i := 0; i < len(nums) - 1; i++{
-        answer = max(answer, nums[i+1] - nums[i], (nums[i+1] - nums[i]) * -1)
+        answer = max(answer, math.Abs(float64(nums[i+1] - nums[i])))
     }     
-    return answer
+    return int(answer)
 }
