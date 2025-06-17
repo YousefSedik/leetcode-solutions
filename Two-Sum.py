a88ba403df195a709ed5f8d2123f2366707a890c
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-06-17 19:19:44 +0300

    [1] [Time Beats: 50.68%] [Memory Beats: 87.16%] - LeetPush

---
+++ Two-Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        nums_original = nums.copy()
+        nums.sort()
+        p1, p2 = 0, len(nums)-1
+        fnum, snum = 0, 0
+        while p1 < p2:
+            if nums[p1] + nums[p2] == target:
+                fnum = nums[p1]
+                snum = nums[p2]
+                break
+            elif nums[p1] + nums[p2] < target:
+                p1 += 1
+            else:
+                p2 -= 1
+        answer = []
+        for i in range(len(nums)):
+            if nums_original[i] == fnum:
+                answer.append(i)
+                fnum = None
+            elif nums_original[i] == snum:
+                answer.append(i)
+                snum = None
+
+        return answer
