c6e5b6a7875dbe993d9f82816bf342f3bcfab6f2
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-06-24 21:57:02 +0300

    [2200] [Time Beats: 100.00%] [Memory Beats: 21.05%] - LeetPush

--- Find-All-K-Distant-Indices-in-an-Array.go
+++ Find-All-K-Distant-Indices-in-an-Array.go
@@ -1,29 +1,24 @@
 func findKDistantIndices(nums []int, key int, k int) []int {
-    result := make([]int, 0)
-    addedIndices := make(map[int]bool)
-    for i := 0; i < len(nums) ; i++{
-        if nums[i] == key{
-            for j := i-1; j > max(i - k - 1, -1); j--{
-                if addedIndices[j] == true{
-                    continue
-                }
-                addedIndices[j] = true
-                result = append(result, j)
-            }
-            if addedIndices[i] == false{
-                addedIndices[i] = true
-                result = append(result, i)
-            }
-            for j := i+1; j < min(i + k + 1, len(nums)); j++{
-                if addedIndices[j] == true{
-                    continue
-                }
-                addedIndices[j] = true
-                result = append(result, j)
-            }
-        }
-    }
+	keyIndices := make([]int, 0)
+	result := make([]int, 0)
 
-    sort.Ints(result)
-    return result
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == key {
+			keyIndices = append(keyIndices, i)
+		}
+	}
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if keyIndices[j]+k < i && j != len(keyIndices) - 1{
+			j++
+		}
+		if i >= keyIndices[j]-k && i <= keyIndices[j]+k{
+			result = append(result, i)
+		}
+		if j != len(keyIndices) - 1 && keyIndices[j+1] == i {
+			j++
+		}
+	}
+
+	return result
 }
