db359196516181dbf7c98182b8c6ec09b8a337b5
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-07-01 02:17:20 +0300

    [279] [Time Beats: 5.01%] [Memory Beats: 16.52%] - LeetPush

---
+++ Perfect-Squares.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        options = [i ** 2 for i in range(1, 101)]
+        mem = defaultdict(lambda: -1)
+        def solve(sum):
+            if sum == 0:
+                return 1
+            elif sum < 0:
+                return 0
+            if mem[sum] != -1:
+                return mem[sum]
+            mi = float("inf")
+            for option in options:
+                ret = solve(sum - option)
+                if ret:
+                    mi = min(mi, 1 + ret)
+            mem[sum] = mi
+            return mi
+
+        return solve(n) - 1
