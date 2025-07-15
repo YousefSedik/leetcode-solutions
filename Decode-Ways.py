cfe0baf049c13a63255194cd0d76c187403abc75
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2025-07-16 01:21:28 +0300

    [91] [Time Beats: 100.00%] [Memory Beats: 52.49%] - LeetPush

--- Decode-Ways.py
+++ Decode-Ways.py
@@ -1,30 +1,24 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        cur_s = [l for l in s]
         mem = dict()
-        def solve(cur_s: list[str]): # TODO: use i instead of using cur_s
-            if len(cur_s) == 0:
+
+        def solve(i: int) -> int:
+            if i == len(s):
                 return 1
-            # is it possible to choose 1 digit ?
-            # yes, if cur_s[0] != 0
-            # is it possible to choose 2 digit ?
-            # yes, if cur_s[0] != 0 and cur_s[0] + cur[1] > 26
-            if mem.get(''.join(cur_s)):
-                return mem.get("".join(cur_s))
-            can_choose_1_d = cur_s[0] != '0'
-            can_choose_2_d = (
-                cur_s[0] != '0' and len(cur_s) >= 2 and int(cur_s[0] + cur_s[1]) <= 26
-            )
-            opt1, opt2 = 0, 0
-            cur_s_c = cur_s.copy()
-            cur_s_c.pop(0)
-            if can_choose_1_d:
-                opt1 = solve(cur_s=cur_s_c)
 
-            if can_choose_2_d:
-                cur_s_c.pop(0)
-                opt2 = solve(cur_s=cur_s_c)
-            mem["".join(cur_s)] = opt1 + opt2
-            return opt1 + opt2
+            if s[i] == '0':
+                return 0
 
-        return solve(cur_s)
+            if i in mem:
+                return mem[i]
+
+            opt1 = solve(i + 1)
+
+            opt2 = 0
+            if i + 1 < len(s) and int(s[i:i+2]) <= 26:
+                opt2 = solve(i + 2)
+
+            mem[i] = opt1 + opt2
+            return mem[i]
+
+        return solve(0)
