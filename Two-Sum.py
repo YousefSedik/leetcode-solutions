fcfe23ee80deed3e034e18811676a358d3e05fdc
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-01-11 17:48:56 +0200

    [1] [Time Beats: 100.00%] [Memory Beats: 65.14%] - LeetPush

--- Two-Sum.py
+++ Two-Sum.py
@@ -1,27 +1,25 @@
-class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        nums_original = nums.copy()
-        nums.sort()
-        p1, p2 = 0, len(nums)-1
-        fnum, snum = 0, 0
-        while p1 < p2:
-            if nums[p1] + nums[p2] == target:
-                fnum = nums[p1]
-                snum = nums[p2]
-                break
-            elif nums[p1] + nums[p2] < target:
-                p1 += 1
-            else:
-                p2 -= 1
-        
-        answer = []
-        for i in range(len(nums)):
-            if nums_original[i] == fnum:
-                answer.append(i)
-                fnum = None
-        
-            elif nums_original[i] == snum:
-                answer.append(i)
-                snum = None
-
-        return answer
+1class Solution:
+2    def twoSum(self, nums: list[int], target: int) -> list[int]:
+3        nums_original = nums.copy()
+4        nums.sort()
+5        p1, p2 = 0, len(nums)-1
+6        fnum, snum = 0, 0
+7        while p1 < p2:
+8            if nums[p1] + nums[p2] == target:
+9                fnum = nums[p1]
+10                snum = nums[p2]
+11                break
+12            elif nums[p1] + nums[p2] < target:
+13                p1 += 1
+14            else:
+15                p2 -= 1
+16        answer = []
+17        for i in range(len(nums)):
+18            if nums_original[i] == fnum:
+19                answer.append(i)
+20                fnum = None
+21            elif nums_original[i] == snum:
+22                answer.append(i)
+23                snum = None
+24
+25        return answer
