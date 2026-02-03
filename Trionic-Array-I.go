a65f075731692bdd87b222e515df145020a73cfa
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-02-03 18:29:09 +0200

    [3637] [Time Beats: 100.00%] [Memory Beats: 30.00%] - LeetPush

--- Trionic-Array-I.go
+++ Trionic-Array-I.go
@@ -1,1 +1,27 @@
-func isTrionic(nums []int) bool {    decIndexStart, decIndexEnd := -1, -1     for i := 0; i < len(nums)-1; i++ {        if nums[i] >= nums[i+1]{            decIndexStart = i            break        }    }    if decIndexStart == 0 || decIndexStart == len(nums) - 1 || decIndexStart == -1 {        return false    }    for i := len(nums) -1 ; i > 0; i-- {        if nums[i-1] >= nums[i]{            decIndexEnd = i
+func isTrionic(nums []int) bool {
+	decIndexStart, decIndexEnd := -1, -1 
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] >= nums[i+1]{
+			decIndexStart = i
+			break
+		}
+	}
+	if decIndexStart == 0 || decIndexStart == len(nums) - 1 || decIndexStart == -1 {
+		return false
+	}
+	for i := len(nums) -1 ; i > 0; i-- {
+		if nums[i-1] >= nums[i]{
+			decIndexEnd = i
+			break
+		}
+	}
+	if decIndexEnd == len(nums) - 1 {
+		return false
+	}
+	for i := decIndexStart; i < decIndexEnd; i++ {
+		if nums[i] <= nums[i+1]{
+			return false
+		}
+	}
+	return true
+}
