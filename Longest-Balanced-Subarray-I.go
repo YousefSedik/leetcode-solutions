9ba98aef0076544216e405c1485faab4d6ff0cc6
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-02-10 19:17:50 +0200

    [3719] [Time Beats: 40.00%] [Memory Beats: 60.00%] - LeetPush

---
+++ Longest-Balanced-Subarray-I.go
@@ -0,0 +1,26 @@
+func longestBalanced(nums []int) int {
+	n := len(nums)
+	globalAnswer := 0
+	localAnswer := 0
+
+	for i := 0; i < n; i++ {
+		Exists := make(map[int]bool)
+		evenCounter, oddCounter := 0, 0
+		for j := i; j < n; j++ {
+			if !Exists[nums[j]] {
+				if nums[j]%2 == 0 {
+					evenCounter++
+				} else {
+					oddCounter++
+				}
+				Exists[nums[j]] = true
+
+			}
+			if evenCounter == oddCounter {
+				localAnswer = j - i + 1
+			}
+		}
+		globalAnswer = max(globalAnswer, localAnswer)
+	}
+	return globalAnswer
+}
