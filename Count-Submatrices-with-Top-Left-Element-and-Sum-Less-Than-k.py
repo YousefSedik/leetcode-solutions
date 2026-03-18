0375c3e9481c6ea6597135e96f82ddaa0c529ad1
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-03-18 17:48:16 +0200

    [3070] [Time Beats: 45.52%] [Memory Beats: 60.00%] - LeetPush

---
+++ Count-Submatrices-with-Top-Left-Element-and-Sum-Less-Than-k.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countSubmatrices(self, grid: List[List[int]], k: int) -> int:
+        counter = 0
+        rows, cols = len(grid), len(grid[0])
+        partial_sum = [[0] * (cols + 1) for _ in range(rows + 1)]
+        
+        for i in range(1, rows + 1):
+            for j in range(1, cols + 1):
+                partial_sum[i][j] = (
+                    grid[i - 1][j - 1]
+                    + partial_sum[i - 1][j] 
+                    + partial_sum[i][j - 1]
+                    - partial_sum[i - 1][j - 1]
+                )
+        
+        for i in range(rows):
+            for j in range(cols):
+                if partial_sum[i+1][j+1] <= k:
+                    counter += 1
+        
+        return counter
