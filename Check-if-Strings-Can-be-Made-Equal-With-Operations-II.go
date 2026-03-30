ac7488f1145c719d9062a896de940f35939b7095
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-03-30 06:03:30 +0200

    [2840] [Time Beats: 100.00%] [Memory Beats: 76.47%] - LeetPush

--- Check-if-Strings-Can-be-Made-Equal-With-Operations-II.go
+++ Check-if-Strings-Can-be-Made-Equal-With-Operations-II.go
@@ -6,15 +6,14 @@ func checkStrings(s1 string, s2 string) bool {
 
 	for i := 0; i < min(len(s1), 2); i++ {
 
-		freqS1 := make(map[rune]int)
-		freqS2 := make(map[rune]int)
+		freqS1 := [26]int{}
+		freqS2 := [26]int{}
 		
 		for j := i; j < len(s1); j+=2 {
-			freqS1[rune(s1[j])]++
+			freqS1[rune(s1[j])-97]++
+			freqS2[rune(s2[j])-97]++
 		}
-		for j := i; j < len(s2); j+=2 {
-			freqS2[rune(s2[j])]++
-		}
+	
 		for k, v := range freqS1 {
 			if freqS2[k] != v {
 				return false
