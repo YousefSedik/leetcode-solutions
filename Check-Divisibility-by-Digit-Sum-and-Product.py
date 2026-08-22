6c93b134e23d1e81f0fdeea17861af840883593f
Yousef Sedik <86931453+YousefSedik@users.noreply.github.com>
2026-08-22 17:36:59 +0300

    [3622] [Time Beats: 13.14%] [Memory Beats: 5.35%] - LeetPush

--- Check-Divisibility-by-Digit-Sum-and-Product.py
+++ Check-Divisibility-by-Digit-Sum-and-Product.py
@@ -1,3 +1,4 @@
 class Solution:
     def checkDivisibility(self, n: int) -> bool:
-        return (n % (sum(int(i) for i in str(n)) + math.prod(int(i) for i in str(n)))) == 0
+        digits = [int(i) for i in str(n)]
+        return (n % (sum(digits) + math.prod(digits))) == 0
